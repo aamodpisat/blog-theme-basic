89c12e87b84a28411d0ac2e99398dd9a44ec3685
Aamod Pisat <aamod.pisat@raweng.com>
2016-08-26 10:26:26 +0530

    basic theme

---
+++ themes/basic/hooks/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Aamod Pisat on 12-08-2016.
+ */
+
+var Hooks = {};
+Hooks.serverExtends = function(app) {
+    app.use('/blog', function(req, res, next){
+        Blog.getRecentPosts()
+            .spread(function success(entries){
+                req.getViewContext().set('recentPosts', entries);
+                next();
+            });
+    }, function(req, res, next){
+        Blog.getAuthors()
+            .spread(function success(entries){
+                req.getViewContext().set('authors', entries);
+                next();
+            });
+    }, function(req, res, next){
+        Blog.getCategories()
+            .spread(function success(entries){
+                req.getViewContext().set('categories', entries);
+                next();
+            });
+    });
+};
+Hooks.templateExtends = function(engine) {
+};
+module.exports =  Hooks;
